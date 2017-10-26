5813b938a2e46bbacab73276185e5d4a8e594f28
SamuliHemminki <samuli.hemminki@cs.helsinki.fi>
2017-10-26 11:51:54 +0300

    added splitToFramesByTime; getTimedomainFeatures

--- @MotionHR/MotionHR.m
+++ @MotionHR/MotionHR.m
@@ -206,59 +206,17 @@ classdef MotionHR
                 gyr = motion(:, obj.DataIndex.gyro);
                 rep = obj.getMotionRepresentations(t, acc, gyr);
                 
+                % Extract features
                 
+                
+                accWithTime = [t,acc];
+                
+                obj.splitToFramesByTime(accWithTime, 5);
                 
             end
             
         end
-        
-        % Get MotionData representatios
-        function representations = getMotionRepresentations(obj, t, acc, gyr)
 
-            % Perform gravity estimation
-            grav = obj.getGravityWithMahony(t, acc, gyr);
-            
-            % Rotate measurements
-            racc  = obj.rotateData(grav, acc);
-            rlacc = obj.rotateData(grav, acc - grav);
-            rgyr  = obj.rotateData(grav, gyr);
-            
-            % Get Representations
-            lacc            = acc-grav;                       % a_lacc
-            [~, accPca]     = pca(acc);                       % a_pca                
-            accMagn         = getMagnitude(acc);              % a_magn
-            [~, laccPca]    = pca(lacc);                      % al_pca1-3
-            laccMagn        = getMagnitude(lacc);             % al_magn
-            rlacc_v         = rlacc(:,3);                     % ar_v
-            [~,rlacc_h_Pca] = pca(rlacc(:,1:2));              % ar_h_pca1-2
-            rlacc_h_Magn    = getMagnitude(rlacc(:,1:2));     % ar_h_magn
-            [~,totG]        = pca(grav);                      % totgrav
-            [~,gyrPca]      = pca(gyr);                       % g_pca1-3
-            gyrMagn         = getMagnitude(gyr);              % g_magn
-            rgyr_v          = rgyr(:,3);                      % gr_v
-            [~,rgyr_h_Pca]  = pca(rgyr(:,1:2));               % gr_h_pca1-2
-            rgyr_h_Magn     = getMagnitude(rgyr(:,1:2));      % gr_h_magn
-
-            % Wrap as structure
-            representations.a_lacc      = lacc;
-            representations.a_pca       = accPca;
-            representations.a_magn      = accMagn;
-            representations.al_pca      = laccPca;
-            representations.al_magn     = laccMagn;
-            representations.alr_v       = rlacc_v;
-            representations.alr_h_pca   = rlacc_h_Pca;
-            representations.alr_h_magn  = rlacc_h_Magn;
-            representations.totgrav     = totG(:,1);
-            representations.g_pca       = gyrPca;
-            representations.g_magn      = gyrMagn;
-            representations.gr_v        = rgyr_v;
-            representations.gr_h_pca    = rgyr_h_Pca;
-            representations.gr_h_magn   = rgyr_h_Magn;
-            
-        end
-        
-        % 
-        
     end
         
     methods (Access = private)
@@ -368,16 +326,95 @@ classdef MotionHR
         
         % Get data for activity
         function activityData = getDataForActivity(obj, activityID, data)
-            
             i_activity   = find(obj.activities.id == activityID, 1, 'first');
             t_start      = obj.activities.ts_start(i_activity);
             t_end        = obj.activities.ts_end(i_activity);
-            
             t            = data(:, obj.DataIndex.ts);
             activityData = data(t>=t_start & t<=t_end, :);
+        end
+     
+        % Calculate basic timedomain features
+        function features = getTimedomainFeatures(obj, input)
+            features = [                       ...
+                mean(input),                  ...
+                median(input),                ...
+                std(input),                   ...
+                var(input),                   ...
+                min(input),                   ...
+                max(input),                   ...
+                range(input),                 ...
+                valueCross(input, 'zero'),    ...
+                valueCross(input, 'mean')];  
+        end
+        
+        % Calculate Frequencydomain features
+        function features = getFrequencydomainFeatures(obj, input) 
+            
             
         end
         
+        % Split data to frames of specified duration
+        function frames = splitToFramesByTime(obj, input, t_frame) 
+            t       = input(:, obj.DataIndex.ts);
+            data    = input(:, 2:end);
+            n       = length(t);
+            i_start = 1;
+            i       = 1;
+            
+            while i_start < n
+                t_start     = t(i_start);
+                i_end       = min([n, find(t > (t_start + t_frame), 1, 'first')]);
+                frames{i}   = data(i_start:i_end,:);
+                i_start     = i_end+1;
+                i           = i+1;
+            end     
+        end
+        
+        % Get motion representatios
+        function representations = getMotionRepresentations(obj, t, acc, gyr)
+
+            % Perform gravity estimation
+            grav = obj.getGravityWithMahony(t, acc, gyr);
+            
+            % Rotate measurements
+            racc  = obj.rotateData(grav, acc);
+            rlacc = obj.rotateData(grav, acc - grav);
+            rgyr  = obj.rotateData(grav, gyr);
+            
+            % Get Representations
+            lacc            = acc-grav;                       % a_lacc
+            [~, accPca]     = pca(acc);                       % a_pca                
+            accMagn         = getMagnitude(acc);              % a_magn
+            [~, laccPca]    = pca(lacc);                      % al_pca1-3
+            laccMagn        = getMagnitude(lacc);             % al_magn
+            rlacc_v         = rlacc(:,3);                     % ar_v
+            [~,rlacc_h_Pca] = pca(rlacc(:,1:2));              % ar_h_pca1-2
+            rlacc_h_Magn    = getMagnitude(rlacc(:,1:2));     % ar_h_magn
+            [~,totG]        = pca(grav);                      % totgrav
+            [~,gyrPca]      = pca(gyr);                       % g_pca1-3
+            gyrMagn         = getMagnitude(gyr);              % g_magn
+            rgyr_v          = rgyr(:,3);                      % gr_v
+            [~,rgyr_h_Pca]  = pca(rgyr(:,1:2));               % gr_h_pca1-2
+            rgyr_h_Magn     = getMagnitude(rgyr(:,1:2));      % gr_h_magn
+
+            % Wrap as structure
+            representations.a_lacc      = lacc;
+            representations.a_pca       = accPca;
+            representations.a_magn      = accMagn;
+            representations.al_pca      = laccPca;
+            representations.al_magn     = laccMagn;
+            representations.alr_v       = rlacc_v;
+            representations.alr_h_pca   = rlacc_h_Pca;
+            representations.alr_h_magn  = rlacc_h_Magn;
+            representations.totgrav     = totG(:,1);
+            representations.g_pca       = gyrPca;
+            representations.g_magn      = gyrMagn;
+            representations.gr_v        = rgyr_v;
+            representations.gr_h_pca    = rgyr_h_Pca;
+            representations.gr_h_magn   = rgyr_h_Magn;
+            
+        end
+
         % Gravity estimation with Mahony 
         % Part of project "On Attitude Estimation with Smartphones" 
         % URL: http://tyrex.inria.fr/mobile/benchmarks-attitude
@@ -448,7 +485,6 @@ classdef MotionHR
             end
             
         end
-  
         
     end
     
